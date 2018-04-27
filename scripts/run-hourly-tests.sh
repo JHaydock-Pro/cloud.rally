6f6e408463017bedc689a3e5cdf9c9fbf8828d39
root <root@vm247.nubes.stfc.ac.uk>
2018-04-27 15:09:10 +0100

    updated scripts

---
+++ scripts/run-hourly-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/local/bin/rally-run-test /etc/openstack-utils/rally.d/cinder_create-and-delete-snapshot.json
+/usr/local/bin/rally-run-test /etc/openstack-utils/rally.d/glance_create-and-list-image.json
+/usr/local/bin/rally-run-test /etc/openstack-utils/rally.d/keystone_create-tenant-with-users.json
+/usr/local/bin/rally-run-test /etc/openstack-utils/rally.d/neutron_create-and-list-floating-ips.json
+/usr/local/bin/rally-run-test /etc/openstack-utils/rally.d/nova_list-images.json
+/usr/local/bin/rally-run-test /etc/openstack-utils/rally.d/vm_boot-runcommand-delete.json
